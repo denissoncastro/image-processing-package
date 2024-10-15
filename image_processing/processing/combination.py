23e2a8e2c54e5ed86d7f26b0d29443641d135af8
dc_alagoano <105017068+denissoncastro@users.noreply.github.com>
2024-10-15 17:53:09 -0300

    Update combination.py

--- image_processing/processing/combination.py
+++ image_processing/processing/combination.py
@@ -4,11 +4,11 @@ from skimage.exposure import match_histograms
 from skimage.metrics import structural_similarity
 
 def find_difference(image1, image2):
-    assert image1.shape == image2.shape, "Specify 2 images with de same shape."
+    assert image1.shape == image2.shape, "Especifique 2 imagens com o mesmo formato."
     gray_image1 = rgb2gray(image1)
     gray_image2 = rgb2gray(image2)
     (score, difference_image) = structural_similarity(gray_image1, gray_image2, full=True)
-    print("Similarity of the images:", score)
+    print("Semelhança das imagens:", score)
     normalized_difference_image = (difference_image-np.min(difference_image))/(np.max(difference_image)-np.min(difference_image))
     return normalized_difference_image
 
